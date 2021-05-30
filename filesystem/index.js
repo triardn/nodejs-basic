7adbac889f9b1e56b9a8b8496d81e5a9baf8539a
Tri Ardini <tr.ardn@gmail.com>
2021-05-30 20:51:39 +0700

    filesystem

---
+++ filesystem/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+const fileReadCallback = (error, data) => {
+    if (error) {
+        console.log('Gagal membaca file')
+
+        return
+    }
+
+    console.log(data)
+}
+
+fs.readFile(path.resolve(__dirname, 'notes.txt'), 'UTF-8', fileReadCallback)
+
+const data = fs.readFileSync(path.resolve(__dirname, 'notes.txt'), 'UTF-8')
+
+console.log(data)
